b9fcd5e1c93532d9095150289bbd317a9612e75a
JasmineRowe <jasrowe@hotmail.com>
2024-10-31 19:12:33 +0000

    lesson update and mocking exercise

--- week-7/W7-S2-mocking/1_Python-Mocking-Intro/exercise/mocking_exercise.py
+++ week-7/W7-S2-mocking/1_Python-Mocking-Intro/exercise/mocking_exercise.py
@@ -12,27 +12,33 @@ def fetch_data(url):
 
 def test_fetch_data():
     """Test the fetch_data function without mocking (not recommended)."""
-    url = "https://catfact.ninja/fact"
+    url = "https://api.agify.io?name=meelad"
     data = fetch_data(url)
-    assert data['id'] == 1  
+
+    assert data['count'] == 21
+    assert data['age'] == 34 
+    assert data['name'] == "meelad" # Check that the fetched data is correct
 
 
 from unittest.mock import patch
 
 @patch('requests.get')
 def test_fetch_data_mocked(mock_get):
+
     """Test the fetch_data function with mocking."""
     # Arrange: Set up the mock to return a specific response
-    mock_get.return_value.json.return_value = {'id': 1, 'title': 'Mocked Title'}
+    mock_get.return_value.json.return_value = {'count': 21, 'age' : 34, 'name' : 'meelad'}
     
     # Act: Call the function under test
-    url = "https://catfact.ninja/fact"
+    url = "https://api.agify.io?name=meelad"
     data = fetch_data(url)
 
-    # Assert: Check the results
-    assert data['id'] == 1
-    assert data['title'] == 'Mocked Title'
+    
+   # Assert: Check the results
+    assert data['count'] == 21
+    assert data[ 'age'] == 34
     mock_get.assert_called_once_with(url)  # Ensure the mock was called with the correct URL
 
+
 # To run the tests in this file, use the following command:
-# $ pytest example.py
+# # $ pytest mocking_example.py
